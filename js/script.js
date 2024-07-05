7b07ddc66b69dd72e200a6dccf0e928f20482728
Kvn-OG <keven.oliveira2k22@gmail.com>
2024-07-05 15:45:28 -0300

    added condictional structure (switch..case) and objects (ibm_values)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const bmi_values = {
+    underWeight: {min:18.5},
+    normalWeight: {min:18.6, max:24.9},
+    overWeight: {min:25, max:29.9},
+    obesityGradeI: {min:30, max:34.9},
+    obesityGradeII: {min:35, max:39.9},
+    obesityGradeIII: {min:40},
+}
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event){
@@ -15,7 +24,26 @@ form.addEventListener('submit', function(event){
 
     document.getElementById('infos').classList.remove('hidden');
     
-    
+    switch(true){
+        case imc < underWeight.min:
+            description = 'Cuidado! Você está abaixo do peso.'
+            break
+        case imc >= normalWeight.min && imc <= normalWeight.max:
+            description = 'Você está no seu peso ideal.'
+            break
+        case imc >= overWeight.min && imc <= overWeight.max:
+            description = 'Você está acima do seu peso ideal.'
+            break
+        case imc >= obesityGradeI.min && imc <= obesityGradeI.max:
+            description = 'Obesidade Grau I'
+            break
+        case imc >= obesityGradeII.min && imc <= obesityGradeII.max:
+            description = 'Obesidade Grau II'
+            break
+        case imc >= obesityGradeIII.min:
+            description = 'Obesidade Grau III (Mórbida)'
+            break
+    }
 
     value.textContent = imc.replace('.',',');
     document.getElementById('description').textContent = description;
