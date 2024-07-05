c3ced6b3bf935a822a98b9df7888a807eb83037c
Kvn-OG <keven.oliveira2k22@gmail.com>
2024-07-04 22:46:38 -0300

    deleted code if

--- js/script.js
+++ js/script.js
@@ -15,24 +15,6 @@ form.addEventListener('submit', function(event){
 
     document.getElementById('infos').classList.remove('hidden');
     
-     // change to swtich case heheheh
-    if(imc < 18.5){
-        description = 'Cuidado!! Você está abaixo do peso!';
-    } else if(imc >= 18.5 && imc <= 25){
-        description = 'Você está no peso ideal';
-        value.classList.remove('attention');
-        value.classList.add('normal');
-    } else if(imc >= 25 && imc <= 30){
-        description = 'Você está acima do peso';
-    } else if(imc >= 30 && imc <= 35){
-        'Obesidade Grau I';
-    } else if(imc >= 35 && imc <= 40){
-        description = 'Obesidade Grau II';
-    } else if(imc > 40){
-        description = 'Obesidade Grau III';
-    } else{
-        description = 'Cuidado! Você está com obesidade mórbida.';
-    }
     
 
     value.textContent = imc.replace('.',',');
