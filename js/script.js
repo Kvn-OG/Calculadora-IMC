d8f59f1c971ff1845e8299c50c42b1b47e9c6179
Kvn-OG <keven.oliveira2k22@gmail.com>
2024-07-06 14:57:12 -0300

    added 'const height' that was accidentally removed on the last commit

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
     
     const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
     const imc = (weight / (height*height)).toFixed(2);
 
     const value = document.getElementById('value');
